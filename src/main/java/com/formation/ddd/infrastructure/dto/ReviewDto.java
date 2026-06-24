2e489f2c6ca8128efe930c6d475280c8e3cca1c7
Jérémy FERNANDES <jeremy.fernandes@zenika.com>
2026-06-24 09:10:28 +0200

    change DTO

--- src/main/java/com/formation/ddd/infrastructure/dto/ReviewDto.java
+++ src/main/java/com/formation/ddd/infrastructure/dto/ReviewDto.java
@@ -1,28 +1,11 @@
 package com.formation.ddd.infrastructure.dto;
 
-import com.formation.ddd.domain.enums.TagEnum;
-import com.formation.ddd.domain.enums.ValidationEnum;
 import com.formation.ddd.domain.model.Author;
 import com.formation.ddd.domain.model.Rating;
 
-import java.time.LocalDateTime;
 
-public record ReviewDto(Long id,
-
-                        String comment,
-
+public record ReviewDto(String comment,
                         Rating rating,
-
-                        int countReport,
-
-                        Author author,
-
-                        LocalDateTime createdAt,
-
-                        TagEnum isTagged,
-
-                        boolean isNotificationSent,
-
-                        ValidationEnum isValidated) {
-
+                        // Faut une DTO
+                        Author author) {
 }
